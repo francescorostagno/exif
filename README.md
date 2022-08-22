288b607b52d154f3e0a2b0ae2681e7a7a8be30b7
Francesco Rostagno <francesco@join-italia.com>
2022-08-22 11:01:51 +0200

    Correzioni e aggiunta del file README

---
+++ README.md
@@ -0,0 +1,13 @@
+EXIF EXPRESS APP: 
+
+Applicazione scritta in expressjs(https://expressjs.com/it/) e nodejs per la visualizzazione dei dati exif di un immagine. 
+
+NB: per poter utilizzare l'applicazione è necessario avere installato node sul pc o sul mac(https://nodejs.org/it/).
+
+Una volta scaricato il pacchetto è necessario installare le dipensenze attraverso il comando npm install. 
+
+Una volta installate per avviare l'applicazione è necessario utilizzare il comando node ./bin/www
+
+E' possibile caricare le proprie foto e visualizzare i dati exif dell'immagine. Una volta caricata l'immagine viene salvata cosi da poterla poi visualizzare in un secondo momento. 
+
+E' possibile ruotare l'immagine e salvarla successivamente ruotata. 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const exif = require('exif-parser')
 const multer = require('multer');
+const _ = require('lodash');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function(req, file, callback) {
@@ -14,10 +16,30 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 const fs = require('fs')
+
+const imageExt = [
+    '.jpg',
+    '.jpeg',
+    '.png',
+    '.gif'
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let images = [];
+  fs.readdir('./public/images/',function (err,files){
+    if(err){
+      console.log(err);
+      res.render('index', { title: 'CIAO',images: images});
+    }else{
+      _.forEach(files,function(file){
+        if( _.includes(imageExt,path.extname(file))){
+          images.push(file);
+        }
+      })
+      res.render('index', { title: 'EXIF APP',images: images});
+    }
+  });
 
-  res.render('index', { title: 'CIAO'});
 });
 
 router.post('/upload', upload.single('image'), async function (req, res) {
@@ -40,6 +62,41 @@ router.post('/upload', upload.single('image'), async function (req, res) {
     return res.render('index', { imagePath: serverPath,absolutServerPath: absolutServerPath, exifData: result , orientationImage: orientationImage });
   }
 });
+
+router.post('/save', async function (req, res) {
+  if (!req.body.newImage) {
+    console.log("No file received");
+    return res.send({
+      success: false
+    });
+
+  } else {
+    var base64Data = req.body.newImage.replace(/^data:image\/png;base64,/,"");
+    fs.writeFile("./public/images/" + req.body.name + '.jpg', base64Data, 'base64', function(err) {
+      if(!err){
+        return res.render('index');
+      }
+    });
+  }
+})
+
+router.post('/select',async function (req,res){
+  if(!req.body.selectedImage){
+    console.log("No file selected");
+    return res.send({
+      success: false
+    });
+  }else{
+    const buffer = fs.readFileSync( './public/images/' + req.body.selectedImage);
+    const parser = exif.create(buffer);
+    const result = parser.parse();
+    const serverPath = 'images/' + req.body.selectedImage;
+    const absolutServerPath = 'public/images/'+ req.body.selectedImage
+    delete result['startMarker'];
+    const orientationImage = result['tags']['Orientation'];
+    return res.render('index', { imagePath: serverPath,absolutServerPath: absolutServerPath, exifData: result , orientationImage: orientationImage});
+  }
+})
 
 
 module.exports = router;
